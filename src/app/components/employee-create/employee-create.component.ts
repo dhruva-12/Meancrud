4dc8f8accf6d17098609dd7e278ef8b834eb4711
Dhruva <dhruvashah1221999@gmail.com>
2020-07-28 21:28:11 +0530

    adding files

---
+++ src/app/components/employee-create/employee-create.component.ts
@@ -0,0 +1,73 @@
+import { Router } from '@angular/router';
+import { ApiService } from './../../service/api.service';
+import { Component, OnInit, NgZone } from '@angular/core';
+import { FormGroup, FormBuilder, Validators } from "@angular/forms";
+
+@Component({
+  selector: 'app-employee-create',
+  templateUrl: './employee-create.component.html',
+  styleUrls: ['./employee-create.component.css']
+})
+
+export class EmployeeCreateComponent implements OnInit {  
+  submitted = false;
+  employeeForm: FormGroup;
+  EmployeeProfile:any = ['Finance', 'BDM', 'HR', 'Sales', 'Admin']
+  date = new Date();  
+  maxDate = (new Date().getFullYear()).toString()+"-0"+(new Date().getMonth()+1).toString()+"-"+(new Date().getDate()).toString();
+
+  
+  
+  constructor(
+    public fb: FormBuilder,
+    private router: Router,
+    private ngZone: NgZone,
+    private apiService: ApiService
+  ) { 
+    this.mainForm();
+  }
+
+  ngOnInit() { }
+
+  mainForm() {
+    this.employeeForm = this.fb.group({
+      name: ['', [Validators.required]],
+      address: ['', [Validators.required]],
+      email: ['', [Validators.required, Validators.pattern('[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,3}$')]],
+      designation: ['', [Validators.required]],
+      phoneNumber: ['', [Validators.required, Validators.pattern('^[0-9]+$')]],
+      salary: ['', [Validators.required]],
+      experience: ['', [Validators.required]],
+    })
+  }
+
+  // Choose designation with select dropdown
+  updateProfile(e){
+    this.employeeForm.get('designation').setValue(e, {
+      onlySelf: true
+    })
+  }
+
+  // Getter to access form control
+  get myForm(){
+    return this.employeeForm.controls;
+  }
+  dateChange(event){
+    console.log(event);
+  }
+  onSubmit() {
+    this.submitted = true;
+    if (!this.employeeForm.valid) {
+      return false;
+    } else {
+      this.apiService.createEmployee(this.employeeForm.value).subscribe(
+        (res) => {
+          console.log('Employee successfully created!')
+          this.ngZone.run(() => this.router.navigateByUrl('/employees-list'))
+        }, (error) => {
+          console.log(error);
+        });
+    }
+  }
+
+}
---
+++ src/app/model/employee.ts
@@ -0,0 +1,9 @@
+export class Employee {
+    name: string;
+    address: string;
+    email: string;
+    designation: string;
+    phoneNumber: number;
+    salary: number;
+    experience: number;
+ }
